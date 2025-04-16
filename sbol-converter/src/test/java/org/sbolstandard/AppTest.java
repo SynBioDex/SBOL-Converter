625b4889a2e563034d0b3147fda98d031284801a
goksel <gokselmisirli@yahoo.com>
2025-04-16 15:07:32 +0100

    Added the SBOLTestSuite

--- sbol-converter/src/test/java/org/sbolstandard/AppTest.java
+++ sbol-converter/src/test/java/org/sbolstandard/AppTest.java
@@ -3,6 +3,7 @@ package org.sbolstandard;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.Set;
@@ -11,11 +12,15 @@ import org.junit.jupiter.api.Test;
 import org.sbolstandard.converter.sbol23_31.SBOLDocumentConverter;
 import org.sbolstandard.core2.ComponentDefinition;
 import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SBOLReader;
+import org.sbolstandard.core2.SBOLValidate;
+import org.sbolstandard.core2.SBOLWriter;
 import org.sbolstandard.core2.SequenceOntology;
 import org.sbolstandard.core3.entity.Component;
 //import org.sbolstandard.core3.entity.SBOLDocument;
 import org.sbolstandard.core3.io.SBOLFormat;
 import org.sbolstandard.core3.io.SBOLIO;
+import org.sbolstandard.core3.util.Configuration;
 import org.sbolstandard.core3.vocabulary.ComponentType;
 import org.sbolstandard.core3.vocabulary.Role;
 
@@ -24,55 +29,81 @@ import org.sbolstandard.core3.vocabulary.Role;
  */
 public class AppTest {
 
- 
-    @Test
-    public void SBOL3ComponentConversion() throws Exception {
-    	//https://uriprefix.com/foo/B0034
-    	URI base=URI.create("https://synbiohub.org/public/igem/");
-    	org.sbolstandard.core3.entity.SBOLDocument doc=new org.sbolstandard.core3.entity.SBOLDocument(base);
-    	//Create the RBS component
-    	Component rbs = doc.createComponent("B0034", Arrays.asList(ComponentType.DNA.getUri())); 
-    	rbs.setName("B0034");
-    	rbs.setDescription("RBS (Elowitz 1999)");
-    	rbs.setRoles(Arrays.asList(Role.RBS));
-    	String output=SBOLIO.write(doc, SBOLFormat.TURTLE);	
-    	System.out.println(output);
-    	
-    	org.sbolstandard.converter.sbol31_23.SBOLDocumentConverter converter = new org.sbolstandard.converter.sbol31_23.SBOLDocumentConverter();
-    	SBOLDocument sbol2Doc = converter.convert(doc);
-    	ByteArrayOutputStream stream = new ByteArrayOutputStream();
-    	sbol2Doc.write(stream);
-    	String result = stream.toString("UTF-8");
-    	System.out.println(result);
-        assertTrue(true);
-    }
-    
-	//@Test
-    public void SBOL2ComponentDefinitionConversion() throws Exception {
-    	//URI base=URI.create("https://synbiohub.org/public/igem/");
-    	SBOLDocument sbol2Doc=new SBOLDocument();
-    	String prURI="https://synbiohub.org/public/igem/";		
-		String prPrefix="pr";	
+	@Test
+	public void SBOL3ComponentConversion() throws Exception {
+		// https://uriprefix.com/foo/B0034
+		URI base = URI.create("https://synbiohub.org/public/igem/");
+		org.sbolstandard.core3.entity.SBOLDocument doc = new org.sbolstandard.core3.entity.SBOLDocument(base);
+		// Create the RBS component
+		Component rbs = doc.createComponent("B0034", Arrays.asList(ComponentType.DNA.getUri()));
+		rbs.setName("B0034");
+		rbs.setDescription("RBS (Elowitz 1999)");
+		rbs.setRoles(Arrays.asList(Role.RBS));
+		String output = SBOLIO.write(doc, SBOLFormat.TURTLE);
+		System.out.println(output);
+
+		org.sbolstandard.converter.sbol31_23.SBOLDocumentConverter converter = new org.sbolstandard.converter.sbol31_23.SBOLDocumentConverter();
+		SBOLDocument sbol2Doc = converter.convert(doc);
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		sbol2Doc.write(stream);
+		String result = stream.toString("UTF-8");
+		System.out.println(result);
+		assertTrue(true);
+	}
+
+	// @Test
+	public void SBOL2ComponentDefinitionConversion() throws Exception {
+		// URI base=URI.create("https://synbiohub.org/public/igem/");
+		SBOLDocument sbol2Doc = new SBOLDocument();
+		String prURI = "https://synbiohub.org/public/igem/";
+		String prPrefix = "pr";
 		sbol2Doc.addNamespace(URI.create(prURI), prPrefix);
-		sbol2Doc.setDefaultURIprefix(prURI);	
+		sbol2Doc.setDefaultURIprefix(prURI);
+
+		ComponentDefinition rbs = sbol2Doc.createComponentDefinition("B0034", ComponentType.DNA.getUri());
+		rbs.setName("B0034");
+		rbs.setDescription("RBS (Elowitz 1999)");
+		rbs.setRoles(Set.of(SequenceOntology.RIBOSOME_ENTRY_SITE));
+
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		sbol2Doc.write(stream);
+		String result = stream.toString("UTF-8");
+		System.out.println(result);
+
+		SBOLDocumentConverter converter = new SBOLDocumentConverter();
+		org.sbolstandard.core3.entity.SBOLDocument sbol3Doc = converter.convert(sbol2Doc);
+		result = SBOLIO.write(sbol3Doc, SBOLFormat.TURTLE);
+		System.out.println(result);
+		assertTrue(true);
+	}
+
+	@Test
+	public void TestSuiteComponent() throws Exception {
+		testTestSuiteFile(new File("../SBOLTestSuite/sbol2/ComponentDefinitionOutput.xml"));		
+	}
 		
-    	ComponentDefinition rbs=sbol2Doc.createComponentDefinition("B0034", ComponentType.DNA.getUri());
-    	rbs.setName("B0034");
-    	rbs.setDescription("RBS (Elowitz 1999)");
-    	rbs.setRoles(Set.of(SequenceOntology.RIBOSOME_ENTRY_SITE));
-    	
-    	ByteArrayOutputStream stream = new ByteArrayOutputStream();
-    	sbol2Doc.write(stream);
-    	String result = stream.toString("UTF-8");
-    	System.out.println(result);
-    	
-    	
-    	SBOLDocumentConverter converter = new SBOLDocumentConverter();
-        org.sbolstandard.core3.entity.SBOLDocument sbol3Doc = converter.convert(sbol2Doc);
-        result=SBOLIO.write(sbol3Doc, SBOLFormat.TURTLE);
-        System.out.println(result);
-        assertTrue(true);
-    }
+	public void testTestSuiteFile(File file) throws Exception {
+		Configuration.getInstance().setValidateBeforeSaving(false);
+
+		org.sbolstandard.core2.SBOLDocument doc = SBOLReader.read(file);
+
+		SBOLDocumentConverter converter = new SBOLDocumentConverter();
+		org.sbolstandard.core3.entity.SBOLDocument sbol3Doc = converter.convert(doc);
+
+		org.sbolstandard.converter.sbol31_23.SBOLDocumentConverter converter3_2 = new org.sbolstandard.converter.sbol31_23.SBOLDocumentConverter();
+		org.sbolstandard.core2.SBOLDocument sbol2Doc = converter3_2.convert(sbol3Doc);
+
+		SBOLWriter.write(sbol2Doc, System.out);
+
+		SBOLValidate.compareDocuments("SBOL2in", doc, "SBOL2out", sbol2Doc);
+		int numOfErros=0;
+		for (String error : SBOLValidate.getErrors()) {
+			System.out.println(error);
+			numOfErros++;
+		}
+		assertTrue(numOfErros==0, "Errors in conversion from" + file.getName());
+	}
 	
 	
+
 }
