227b939dec6c552df81270b331d5c023b7be1595
Chris Myers <myers@ece.utah.edu>
2025-04-17 08:08:12 -0600

    Module support

--- sbol-converter/src/main/java/org/sbolstandard/converter/sbol31_23/ComponentConverter.java
+++ sbol-converter/src/main/java/org/sbolstandard/converter/sbol31_23/ComponentConverter.java
@@ -1,5 +1,7 @@
 package org.sbolstandard.converter.sbol31_23;
 
+import java.net.URI;
+
 import org.sbolstandard.converter.Util;
 import org.sbolstandard.core2.ComponentDefinition;
 import org.sbolstandard.core3.entity.Component;
@@ -10,11 +12,19 @@ import org.sbolstandard.core3.util.SBOLGraphException;
 public class ComponentConverter implements EntityConverter<Component, ComponentDefinition>  {
 
     @Override
-    public ComponentDefinition convert(SBOLDocument doc, Component input) throws SBOLGraphException, SBOLValidationException {  
-    	ComponentDefinition comp;
-		comp = doc.createComponentDefinition(Util.getURIPrefix(input),input.getDisplayId(),Util.getVersion(input),Util.toSet(input.getTypes()));
-    	Util.copyIdentified(input, comp);
-//        comp.setRoles(Util.convertRoles(input.getRoles()));
-        return comp;
+    public ComponentDefinition convert(SBOLDocument doc, Component input) throws SBOLGraphException, SBOLValidationException { 
+    	// TODO: no easy way to see if a URI is in the set of roles
+    	if (input.getTypes()!=null) {
+    		for (URI type : input.getTypes()) {
+    			if (type.toString().equals("https://identifiers.org/SBO:0000241")) {
+    				return null;
+    			}
+    		}
+    	}
+    	ComponentDefinition compDef;
+    	compDef = doc.createComponentDefinition(Util.getURIPrefix(input),input.getDisplayId(),Util.getVersion(input),Util.toSet(input.getTypes()));
+    	Util.copyIdentified(input, compDef);
+//      	  comp.setRoles(Util.convertRoles(input.getRoles()));
+        return compDef;
     }
 }
