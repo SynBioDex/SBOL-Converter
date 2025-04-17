2087f465ec8b9c2ef9dc7d6cdf5e6c9d47b16d2d
Ilya <damagedone@yandex.ru>
2025-04-17 19:27:00 +0400

    brackets and style

--- sbol-converter/src/main/java/org/sbolstandard/converter/sbol23_31/ImplementationConverter.java
+++ sbol-converter/src/main/java/org/sbolstandard/converter/sbol23_31/ImplementationConverter.java
@@ -10,9 +10,10 @@ public class ImplementationConverter implements EntityConverter<org.sbolstandard
     @Override
     public Implementation convert(SBOLDocument doc, org.sbolstandard.core2.Implementation input) throws SBOLGraphException { 
     	Implementation impl = doc.createImplementation(Util.createSBOL3Uri(input), Util.getNamespace(input));
-    	Util.copyIdentified(input, impl);
-		if (input.isSetBuilt())
+		Util.copyIdentified(input, impl);
+		if (input.isSetBuilt()) {
 			impl.setComponent(Util.createSBOL3Uri(input.getBuiltURI()));
+		}
 		return impl;
 	}
 }
