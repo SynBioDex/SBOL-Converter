662ee92b7c0623ede5f38f0f66f086c13c9630f2
Metehan Unal <13197858+metehanunal@users.noreply.github.com>
2025-06-13 09:30:57 +0100

    minor changes

--- sbol-converter/src/main/java/org/sbolstandard/converter/sbol23_31/ModuleToSubComponentConverter.java
+++ sbol-converter/src/main/java/org/sbolstandard/converter/sbol23_31/ModuleToSubComponentConverter.java
@@ -1,5 +1,6 @@
 package org.sbolstandard.converter.sbol23_31;
 
+import org.sbolstandard.core2.ModuleDefinition;
 import org.sbolstandard.core2.SBOLValidationException;
 import org.sbolstandard.core3.entity.Identified;
 import org.sbolstandard.core3.entity.SBOLDocument;
@@ -14,8 +15,16 @@ public class ModuleToSubComponentConverter
 			org.sbolstandard.core2.Identified sbol2Parent, org.sbolstandard.core2.Module sbol2Module)
 			throws SBOLGraphException, SBOLValidationException {
 
+		ModuleDefinition sbol2ParentModuleDefinition = (ModuleDefinition) sbol2Parent;
+		org.sbolstandard.core3.entity.Component sbol3ParentComponent = (org.sbolstandard.core3.entity.Component) sbol3Parent;
+		
+		
+		
 		// TODO:
 		// MAPS TOS
+		
+		
+		
 		// MEASURES
 
 		System.out.println("ModuleToSubComponentConverter.convert() NOT IMPLEMENTED YET");
